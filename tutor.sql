bf8fe673626b3e58162fc0e833adfe23039d14a4
giautpvl <giautpvl@gmail.com>
2015-06-17 22:55:46 +0700

    Revert "Revert "CSDL""
    This reverts commit 688c445c1813764694d1e6e96e1c1c4727098546.

---
+++ tutor.sql
@@ -0,0 +1,434 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2015 at 12:05 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tutor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE IF NOT EXISTS `cities` (
+`id` int(11) NOT NULL,
+  `city` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE IF NOT EXISTS `customers` (
+`id` int(11) NOT NULL,
+  `fullName` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `phoneNo` int(11) NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `days`
+--
+
+CREATE TABLE IF NOT EXISTS `days` (
+`id` int(11) NOT NULL,
+  `day` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `days_tutors`
+--
+
+CREATE TABLE IF NOT EXISTS `days_tutors` (
+`id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `day_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `degrees`
+--
+
+CREATE TABLE IF NOT EXISTS `degrees` (
+`id` int(11) NOT NULL,
+  `degree` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts`
+--
+
+CREATE TABLE IF NOT EXISTS `districts` (
+`id` int(11) NOT NULL,
+  `district` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts_tutors`
+--
+
+CREATE TABLE IF NOT EXISTS `districts_tutors` (
+`id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `district_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fields`
+--
+
+CREATE TABLE IF NOT EXISTS `fields` (
+`id` int(11) NOT NULL,
+  `field` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `labels`
+--
+
+CREATE TABLE IF NOT EXISTS `labels` (
+`id` int(11) NOT NULL,
+  `label` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `labels_tutors`
+--
+
+CREATE TABLE IF NOT EXISTS `labels_tutors` (
+`id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `label_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `majors`
+--
+
+CREATE TABLE IF NOT EXISTS `majors` (
+`id` int(11) NOT NULL,
+  `field_id` int(11) NOT NULL,
+  `major` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE IF NOT EXISTS `requests` (
+`id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `numOfDay` int(2) NOT NULL,
+  `numOfPeo` int(2) NOT NULL,
+  `place` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `fee` int(11) DEFAULT NULL,
+  `sex` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests_tutors`
+--
+
+CREATE TABLE IF NOT EXISTS `requests_tutors` (
+`id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `request_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE IF NOT EXISTS `subjects` (
+`id` int(11) NOT NULL,
+  `subject` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects_tutors`
+--
+
+CREATE TABLE IF NOT EXISTS `subjects_tutors` (
+`id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutors`
+--
+
+CREATE TABLE IF NOT EXISTS `tutors` (
+`id` int(11) NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `firstName` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `lastName` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `dateOfBirth` date NOT NULL,
+  `sex` tinyint(1) NOT NULL,
+  `phoneNo` int(11) NOT NULL,
+  `identityCard` int(11) NOT NULL,
+  `address` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `fee` int(11) DEFAULT NULL,
+  `image` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutors_details`
+--
+
+CREATE TABLE IF NOT EXISTS `tutors_details` (
+`id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `major_id` int(11) NOT NULL,
+  `degree_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `days`
+--
+ALTER TABLE `days`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `days_tutors`
+--
+ALTER TABLE `days_tutors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `degrees`
+--
+ALTER TABLE `degrees`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `districts`
+--
+ALTER TABLE `districts`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `districts_tutors`
+--
+ALTER TABLE `districts_tutors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fields`
+--
+ALTER TABLE `fields`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `labels`
+--
+ALTER TABLE `labels`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `labels_tutors`
+--
+ALTER TABLE `labels_tutors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `majors`
+--
+ALTER TABLE `majors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `requests_tutors`
+--
+ALTER TABLE `requests_tutors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects_tutors`
+--
+ALTER TABLE `subjects_tutors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutors`
+--
+ALTER TABLE `tutors`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutors_details`
+--
+ALTER TABLE `tutors_details`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `days`
+--
+ALTER TABLE `days`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `days_tutors`
+--
+ALTER TABLE `days_tutors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `degrees`
+--
+ALTER TABLE `degrees`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `districts`
+--
+ALTER TABLE `districts`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `districts_tutors`
+--
+ALTER TABLE `districts_tutors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fields`
+--
+ALTER TABLE `fields`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `labels`
+--
+ALTER TABLE `labels`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `labels_tutors`
+--
+ALTER TABLE `labels_tutors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `majors`
+--
+ALTER TABLE `majors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `requests_tutors`
+--
+ALTER TABLE `requests_tutors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `subjects_tutors`
+--
+ALTER TABLE `subjects_tutors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tutors`
+--
+ALTER TABLE `tutors`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tutors_details`
+--
+ALTER TABLE `tutors_details`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
